b4daac27ad499fdfebd4166db6063f797ef4d36c
Vaibhav Pant <pantvaibhav16@gmail.com>
2024-07-01 15:45:39 +0530

    created routes and empty components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Results from './components/Results';
+import NoPage from './components/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <route path='/' element={<Layout />}>
+          <route index element={<Home />} />
+          <route path="results/:mal_id" element={<Results />} />
+          <route path element={<NoPage />} />
+        </route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
