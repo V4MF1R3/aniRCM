f178e9a12c0f60d3b8a4150a1a7f4d837d3bc70b
V4MF1R3 <pantvaibhav16@gmail.com>
2024-09-08 16:58:12 +0530

    open port

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def serve_react(path):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
